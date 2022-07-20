10f75d8f35ebb9bca878103f81e675ba522d6a52
Paulina <gulczynskap@gmail.com>
2022-07-20 21:14:37 +0200

    Adding function menu() which prints main menu

--- terminal_login.py
+++ terminal_login.py
@@ -6,3 +6,8 @@ def clear_console():
         os.system('cls')
     else:
         os.system('clear')
+
+
+def menu():
+    clear_console()
+    print("Hello, what do you want to do? Choose one option:\n1. Log in\n2. Sign up\n3. Exit")
